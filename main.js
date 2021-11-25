485a13561ccc64a30e24b5c4048543305d9f7378
0x7FFFFFFFFFFFFFFF <39968+0x7FFFFFFFFFFFFFFF@users.noreply.github.com>
2021-11-25 09:39:47 +0800

    Update main.js

--- main.js
+++ main.js
@@ -11,8 +11,13 @@
 (function() {
     'use strict';
     if (/youtube\.com\/?$/.test(location.href)) {
+        console.log("Youtube home page detected!");
+
         setInterval((function () {
-            let cells = $("ytd-rich-item-renderer.style-scope.ytd-rich-grid-renderer:not(.fucked)");
+            let cells = $("ytd-rich-item-renderer.style-scope.ytd-rich-grid-row:not(.fucked)");
+            if(cells.length == 0) {
+                console.log("Can't find any cells to check");
+            }
             cells.each(function(){
                 let cell = $(this);
                 let temp = cell.find("ytd-thumbnail");
@@ -25,6 +30,31 @@
                     }
                     else if(e.altKey) {
                         $(this).closest("ytd-rich-item-renderer").find("button.style-scope.yt-icon-button").click();
+                        setTimeout(function(){
+                            $(`yt-formatted-string:contains("Don't recommend channel")`).click();
+                        }, 50);
+                    }
+                });
+            });
+            cells.addClass("fucked");
+        }), 2000);
+    }
+
+    if (/youtube\.com\/watch/.test(location.href)) {
+        setInterval((function () {
+            let cells = $("ytd-compact-video-renderer.style-scope.ytd-item-section-renderer:not(.fucked)");
+            cells.each(function(){
+                let cell = $(this);
+                let temp = cell.find("ytd-thumbnail");
+                temp.hover(function(e){
+                    if(e.ctrlKey) {
+                        $(this).closest("ytd-compact-video-renderer").find("button.style-scope.yt-icon-button").click();
+                        setTimeout(function(){
+                            $("yt-formatted-string:contains('Not interested')").click();
+                        }, 50);
+                    }
+                    else if(e.altKey) {
+                        $(this).closest("ytd-compact-video-renderer").find("button.style-scope.yt-icon-button").click();
                         setTimeout(function(){
                             $(`yt-formatted-string:contains("Don't recommend channel")`).click();
                         }, 50);
